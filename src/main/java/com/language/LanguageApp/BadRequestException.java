a5cf3e774c38e9399f5bb24b1f8fe61092ecf539
jweah555 <j_weah@uncg.edu>
2025-08-12 21:43:49 -0400

    Frontend and Backend of the app

--- src/main/java/com/language/LanguageApp/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.language.LanguageApp;
-
-public class BadRequestException extends RuntimeException{
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/language/LanguageApp/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.language.LanguageApp;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
