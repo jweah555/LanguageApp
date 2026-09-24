1509a9e5143666d3ffc3d1532778f23c4b6f90f3
James Weah <122507491+jweah555@users.noreply.github.com>
2026-09-23 21:05:12 -0400

    Added post request from front-end for user to create card

--- LanguageApp-Frontend/src/pages/CreateCard.jsx
+++ LanguageApp-Frontend/src/pages/CreateCard.jsx
@@ -3,23 +3,33 @@ import "../pages/CreateCard.css";
 import { useState } from "react";
 
 const status = "Good"
-const userId = 6;
+const deckId = 7;
 function CreateCard() {
 
-  const [cardFront, setCardFront] = useState("");
-  const [cardBack, setCardBack] = useState("");
-  const [cardLanguage, setCardLanguage] = useState("");
+  //These inputs must match backend fields for json
+  const [front, setFront] = useState("");
+  const [back, setBack] = useState("");
+  const [language, setLanguage] = useState("");
 
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const card = {cardFront, cardBack, cardLangauge, status, userId}
+    const card = {front, back, language, status}
 
-    const response = await fetch("http://localhost:8080/", {
+    const response = await fetch("http://localhost:8080/card/7", {
       method: "POST",
       headers:{ "Content-Type": "application/json"},
       body: JSON.stringify(card),
+      credentials: "include"
     });
+    console.log(card)
+
+    if(response.ok) {
+      setBack("")
+      setFront("")
+      setLanguage("")
+     
+    }
     
   }
 
@@ -32,24 +42,24 @@ function CreateCard() {
         <form  onSubmit={handleSubmit} id="create-card">
           <input  placeholder=" Enter Card Name" className="card-name"></input>
           <input
-            onChange={(e) => setCardLanguage(e.target.value)}
-            value={cardLanguage}
+            onChange={(e) => setLanguage(e.target.value)}
+            value={language}
             placeholder=" Enter Card Language"
             className="card-language"
           ></input>
           <textarea
-            onChange={(e) => setCardFront(e.target.value)}
-            value={cardFront}
+            onChange={(e) => setFront(e.target.value)}
+            value={front}
             placeholder=" Enter Source Language Text"
             className=""
           ></textarea>
           <textarea
-            onChange={(e) => setCardBack(e.target.value)}
-            value={cardBack}
+            onChange={(e) => setBack(e.target.value)}
+            value={back}
             placeholder=" Enter Translated Language Text "
             className=""
           ></textarea>
-          <button className="create-card-button" form="card-card">Create</button>
+          <button onClick={handleSubmit} className="create-card-button" form="create-card">Create</button>
         </form>
       </div>
     </main>
